dad589f7412c0208d5789b3eaaf1b6b5a0c25f26
李文强 <wenqiang.li@wenba100.com>
2020-03-04 19:03:01 +0800

    提交学习代码

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module gin-study-case
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.5.0
+require (
+	github.com/gin-gonic/gin v1.5.0
+	github.com/golang/protobuf v1.3.2
+	google.golang.org/grpc v1.27.1
+)
--- main.go
+++ main.go
@@ -1,25 +1,39 @@
 package main
 
 import (
-	"gin-study-case/handlers/proto_buffers"
-	"gin-study-case/actions/floyd"
-	"github.com/gin-gonic/gin"
+	"gin-study-case/protocal_buffers/todo"
+	"google.golang.org/grpc"
+	"log"
+	"net"
 )
+
+type TaskServer struct {
+
+}
 
 func main() {
 
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200 , gin.H{
-			"message":"pong",
-		})
-	})
+	var tasks TaskServer
 
-	r.POST("/todo/add",proto_buffers.Add)
-
-
-
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-	r.GET("/floyd", floyd.Floyd)
-	r.Run("0.0.0.0:12345") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	svr := grpc.NewServer()
+	todo.RegisterTasksServer(svr , tasks)
+	l , err := net.Listen("tcp","14444")
+	if err != nil {
+		log.Fatalf("Can't listen tcp server with port is 14444 , err is :%v" , err.Error())
+	}
+	svr.Serve(l)
+	//r := gin.Default()
+	//r.GET("/ping", func(c *gin.Context) {
+	//	c.JSON(200 , gin.H{
+	//		"message":"pong",
+	//	})
+	//})
+	//
+	//r.POST("/todo/add",proto_buffers.Add)
+	//
+	//
+	//
+	//r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	//r.GET("/floyd", floyd.Floyd)
+	//r.Run("0.0.0.0:12345") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
