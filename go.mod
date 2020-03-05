34610f9aa2a98a2dedaa2b63432609c8c7a7dd24
zooeymoon <kimmi-moon@163.com>
2020-03-06 01:37:49 +0800

    no message

--- go.mod
+++ go.mod
@@ -4,9 +4,14 @@ go 1.14
 
 require (
 	github.com/gin-gonic/gin v1.5.0
-	github.com/go-redis/redis/v7 v7.2.0
 	github.com/golang/protobuf v1.3.2
 	github.com/jinzhu/gorm v1.9.12
 	github.com/joho/godotenv v1.3.0
+	github.com/kr/pretty v0.1.0 // indirect
+	golang.org/x/net v0.0.0-20190923162816-aa69164e4478 // indirect
+	golang.org/x/sys v0.0.0-20191010194322-b09406accb47 // indirect
+	golang.org/x/text v0.3.2 // indirect
 	google.golang.org/grpc v1.27.1
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
--- handlers/proto_buffers/list.go
+++ handlers/proto_buffers/list.go
@@ -1,63 +1,72 @@
 package proto_buffers
 
 import (
-	"bytes"
-	"encoding/gob"
+	"context"
 	"fmt"
-	"gin-study-case/protocal_buffers/todo"
+	"gin-study-case/protocal_buffers/todo_list"
 	"github.com/gin-gonic/gin"
-	"github.com/golang/protobuf/proto"
-	"io/ioutil"
+	"google.golang.org/grpc"
+	"log"
 )
 
 func List(c *gin.Context) {
-	const dbPath = "./output/mydb.pb"
-	var todoProto = &todo.Task{}
-	var err error
 
-	b, err := ioutil.ReadFile(dbPath)
+	conn, err := grpc.Dial(":14431", grpc.WithInsecure())
 	if err != nil {
-		c.JSON(200, err.Error())
-		return
+		log.Fatalf("can not dial to localhost:14431 , err: %v", err.Error())
 	}
+	client := todo_list.NewTasksClient(conn)
 
-	for {
-
-		var length int32
-
-		if len(b) == 0 {
-			c.JSON(200,"done")
-			return
-		}else if len(b) < 4 {
-			c.JSON(200 , "bytes的长度小于4")
-			return
-		}
-
-		if err = gob.NewDecoder(bytes.NewReader(b[:4])).Decode(&length);err != nil {
-			c.JSON(200 , err.Error())
-			return
-		}
-
-		b = b[4:]
-
-		if err = proto.Unmarshal(b[:length], todoProto); err != nil{
-			c.JSON(200, err.Error())
-			return
-		}
-
-		b = b[length:]
-
-		if todoProto.Done {
-			fmt.Println("no")
-		}else{
-			fmt.Println("yes")
-		}
-
-		c.JSON(200 , todoProto.Text)
+	results, err := client.List(context.Background(), &todo_list.Void{})
+	if err != nil {
+		fmt.Errorf("method list has problem , err: %v", err.Error())
 	}
 
 
-
-
+	c.JSON(200, results)
+	//const dbPath = "./output/mydb.pb"
+	//var todoProto = &todo.Task{}
+	//var err error
+	//
+	//b, err := ioutil.ReadFile(dbPath)
+	//if err != nil {
+	//	c.JSON(200, err.Error())
+	//	return
+	//}
+	//
+	//for {
+	//
+	//	var length int32
+	//
+	//	if len(b) == 0 {
+	//		c.JSON(200,"done")
+	//		return
+	//	}else if len(b) < 4 {
+	//		c.JSON(200 , "bytes的长度小于4")
+	//		return
+	//	}
+	//
+	//	if err = gob.NewDecoder(bytes.NewReader(b[:4])).Decode(&length);err != nil {
+	//		c.JSON(200 , err.Error())
+	//		return
+	//	}
+	//
+	//	b = b[4:]
+	//
+	//	if err = proto.Unmarshal(b[:length], todoProto); err != nil{
+	//		c.JSON(200, err.Error())
+	//		return
+	//	}
+	//
+	//	b = b[length:]
+	//
+	//	if todoProto.Done {
+	//		fmt.Println("no")
+	//	}else{
+	//		fmt.Println("yes")
+	//	}
+	//
+	//	c.JSON(200 , todoProto.Text)
+	//}
 
 }
