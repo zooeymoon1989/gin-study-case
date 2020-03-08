02921fe4dc59da6c6ef280ad2d595a7de0f5b66c
zooeymoon <kimmi-moon@163.com>
2020-03-09 01:30:06 +0800

    no message

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"gin-study-case/actions/concurrecy_go"
 	"gin-study-case/handlers/proto_buffers"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
@@ -17,30 +18,29 @@ func init() {
 	}
 }
 
-
 type TaskServer struct {
 }
 
 func main() {
 
-	connections := os.Getenv("DB_USERNAME")+":"+os.Getenv("DB_PASSWORD")+"@"+os.Getenv("DB_HOST")+"/"+os.Getenv("DB_DATABASE")+"?charset=utf8&parseTime=True&loc=Local"
+	connections := os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@" + os.Getenv("DB_HOST") + "/" + os.Getenv("DB_DATABASE") + "?charset=utf8&parseTime=True&loc=Local"
 
 	//var tasks TaskServer
 	db, err := gorm.Open("mysql", connections)
 	defer db.Close()
-	if err != nil{
-		fmt.Errorf("can not connect to mysql , err : %s" , err.Error())
+	if err != nil {
+		fmt.Errorf("can not connect to mysql , err : %s", err.Error())
 	}
 
 	r := gin.Default()
-	r.POST("/todo/add",proto_buffers.Add)
-	r.POST("/todo/list",proto_buffers.List)
+	r.POST("/todo/add", proto_buffers.Add)
+	r.POST("/todo/list", proto_buffers.List)
+	r.POST("/cg/or_channel", concurrecy_go.OrChannel)
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200 , gin.H{
-			"message":"pong",
+		c.JSON(200, gin.H{
+			"message": "pong",
 		})
 	})
-
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
