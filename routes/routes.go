a20078b58b479995c330e1be300b269204ab7053
zooeymoon <kimmi-moon@163.com>
2020-03-23 02:01:58 +0800

    trace

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"gin-study-case/actions/concurrecy_go"
+	"gin-study-case/actions/trace"
 	"gin-study-case/handlers/proto_buffers"
 	"github.com/gin-gonic/gin"
 )
@@ -16,6 +17,8 @@ func Route() *gin.Engine {
 	r.POST("/error/error_handle",concurrecy_go.ErrorHandle)
 	r.POST("/cg/pipeline",concurrecy_go.Pipeline)
 	r.POST("/cg/repeat",concurrecy_go.Repeat)
+	//trace工具
+	r.POST("/forFunc/trace",trace.Trace)
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
